e3241f0637676dab62e84b86a1cef5b637c4fcd7
Kauã Kelvyn <112822162+Kerubink@users.noreply.github.com>
2024-10-28 13:23:35 -0300

    cam update

--- frontend/src/components/modalsTransfers/Scanner/scanner.jsx
+++ frontend/src/components/modalsTransfers/Scanner/scanner.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import QrScanner from "react-qr-scanner";
+import React, { useState, useEffect, useRef } from "react";
 import jsQR from "jsqr";
 import modalStyles from "../modalGlobal.module.css";
 import scannerStyles from "./scanner.module.css";
@@ -7,19 +6,15 @@ import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRound
 import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
 import UploadIcon from "@mui/icons-material/Upload";
 
-export default function ModalScanner({
-  onClose,
-  onQRCodeRead = () => {},
-  userData,
-}) {
+export default function ModalScanner({ onClose, onQRCodeRead = () => {}, userData }) {
   const [scanResult, setScanResult] = useState("");
   const [useCamera, setUseCamera] = useState(true);
   const [paymentData, setPaymentData] = useState(null);
+  const videoRef = useRef(null); // Referência para o vídeo
 
   const handleScan = (data) => {
     if (data) {
-      const qrData =
-        typeof data === "object" && data !== null ? data.text : data;
+      const qrData = typeof data === "object" && data !== null ? data.text : data;
       setScanResult(qrData);
 
       try {
@@ -58,12 +53,7 @@ export default function ModalScanner({
           canvas.width = imgElement.width;
           canvas.height = imgElement.height;
           context.drawImage(imgElement, 0, 0);
-          const imageData = context.getImageData(
-            0,
-            0,
-            canvas.width,
-            canvas.height
-          );
+          const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
           const qrCode = jsQR(imageData.data, canvas.width, canvas.height);
           if (qrCode) {
             handleScan(qrCode.data);
@@ -97,17 +87,14 @@ export default function ModalScanner({
     };
 
     try {
-      const response = await fetch(
-        "https://projeto-final-m5-api.onrender.com/api/transactions",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-          },
-          body: JSON.stringify(payload),
-        }
-      );
+      const response = await fetch("https://projeto-final-m5-api.onrender.com/api/transactions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+        body: JSON.stringify(payload),
+      });
 
       if (!response.ok) {
         const errorData = await response.json();
@@ -122,43 +109,70 @@ export default function ModalScanner({
     }
   };
 
+  const startCamera = async () => {
+    if (videoRef.current) return; // Se a câmera já estiver sendo usada, não faz nada
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: { exact: "environment" } }, // Usar a câmera traseira
+      });
+      videoRef.current = document.createElement("video");
+      videoRef.current.srcObject = stream;
+      videoRef.current.play();
+      document.body.appendChild(videoRef.current); // Adiciona o vídeo ao DOM
+      requestAnimationFrame(tick);
+    } catch (error) {
+      console.error("Erro ao acessar a câmera:", error);
+    }
+  };
+
+  const tick = () => {
+    if (videoRef.current) {
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      canvas.width = videoRef.current.videoWidth;
+      canvas.height = videoRef.current.videoHeight;
+      context.drawImage(videoRef.current, 0, 0);
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      const qrCode = jsQR(imageData.data, canvas.width, canvas.height);
+      if (qrCode) {
+        handleScan(qrCode.data); // Tenta ler o QR Code
+      }
+      requestAnimationFrame(tick); // Chama a próxima iteração
+    }
+  };
+
+  useEffect(() => {
+    if (useCamera) {
+      startCamera();
+    } else {
+      if (videoRef.current) {
+        const stream = videoRef.current.srcObject;
+        const tracks = stream.getTracks();
+        tracks.forEach((track) => track.stop()); // Para a transmissão da câmera
+        document.body.removeChild(videoRef.current); // Remove o vídeo do DOM
+        videoRef.current = null; // Limpa a referência
+      }
+    }
+  }, [useCamera]);
+
   return (
     <div className={modalStyles.modal}>
       {paymentData ? (
         <div className={scannerStyles.paymentOverlay}>
           <h2 className={scannerStyles.title}>Dados do Pagamento:</h2>
           <div className={`${scannerStyles.paymentInfo} ${scannerStyles.card}`}>
-            <p>
-              <strong>Nome do Recebedor:</strong> {paymentData.userName}
-            </p>
-            <p>
-              <strong>Conta:</strong> {paymentData.accountNumber}
-            </p>
-            <p>
-              <strong>Valor:</strong> R$ {paymentData.value}
-            </p>
-            <p>
-              <strong>Descrição:</strong>{" "}
-              {paymentData.description || "Pagamento via QR Code"}
-            </p>
-            <p>
-              <strong>ID Transação:</strong> {paymentData.transactionId}
-            </p>
-            <p>
-              <strong>Validade:</strong> {paymentData.validUntil}
-            </p>
+            <p><strong>Nome do Recebedor:</strong> {paymentData.userName}</p>
+            <p><strong>Conta:</strong> {paymentData.accountNumber}</p>
+            <p><strong>Valor:</strong> R$ {paymentData.value}</p>
+            <p><strong>Descrição:</strong> {paymentData.description || "Pagamento via QR Code"}</p>
+            <p><strong>ID Transação:</strong> {paymentData.transactionId}</p>
+            <p><strong>Validade:</strong> {paymentData.validUntil}</p>
           </div>
           <div className={scannerStyles.paymentButtons}>
-            <button
-              className={`${scannerStyles.paymentButton} ${scannerStyles.confirmButton}`}
-              onClick={handlePayment}
-            >
+            <button className={`${scannerStyles.paymentButton} ${scannerStyles.confirmButton}`} onClick={handlePayment}>
               Confirmar
             </button>
-            <button
-              className={`${scannerStyles.cancelButton} ${scannerStyles.cancelButton}`}
-              onClick={handleCancelPayment}
-            >
+            <button className={`${scannerStyles.cancelButton} ${scannerStyles.cancelButton}`} onClick={handleCancelPayment}>
               Cancelar
             </button>
           </div>
@@ -167,13 +181,6 @@ export default function ModalScanner({
         <>
           {useCamera ? (
             <>
-              <QrScanner
-                delay={300}
-                onError={handleError}
-                onScan={handleScan}
-                className={scannerStyles.videoBackground}
-                facingMode="environment"
-              />
               <div className={scannerStyles.videoOverlay}></div>
             </>
           ) : (
@@ -184,8 +191,7 @@ export default function ModalScanner({
                 onChange={handleFileChange}
                 className={scannerStyles.fileInput}
               />
-              <span className={scannerStyles.fileInputText}>Carregar Arquivo</span>{" "}
-              {/* Texto opcional */}
+              <span className={scannerStyles.fileInputText}>Carregar Arquivo</span>
             </label>
           )}
 
