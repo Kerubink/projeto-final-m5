e03b28e799c8e2bff70971cc2f4d38ceac554406
Kauã Kelvyn <112822162+Kerubink@users.noreply.github.com>
2024-10-28 13:52:00 -0300

    trocaaaaaa

--- frontend/src/components/modalsTransfers/Scanner/scanner.jsx
+++ frontend/src/components/modalsTransfers/Scanner/scanner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import QrScanner from "react-qr-scanner";
 import jsQR from "jsqr";
 import modalStyles from "../modalGlobal.module.css";
@@ -16,7 +16,7 @@ export default function ModalScanner({
   const [scanResult, setScanResult] = useState("");
   const [useCamera, setUseCamera] = useState(true);
   const [paymentData, setPaymentData] = useState(null);
-  const [cameraFacingMode, setCameraFacingMode] = useState("environment"); // Padrão para câmera traseira
+  const [cameraFacingMode, setCameraFacingMode] = useState("user"); // Padrão para câmera frontal
 
   const handleScan = (data) => {
     if (data) {
@@ -125,9 +125,7 @@ export default function ModalScanner({
   };
 
   const toggleCamera = () => {
-    setCameraFacingMode((prev) =>
-      prev === "user" ? "environment" : "user"
-    );
+    setCameraFacingMode((prev) => (prev === "user" ? "environment" : "user"));
   };
 
   return (
